Fixed computation of gradient

src/grad_desc_clustering.py
@@ -12,13 +12,16 @@ class GDC:
         W = np.random.randn(n, K)
         all_prev = []
         #Train
+        all_dW = np.zeros(epochs)
+        all_dX_bar = np.zeros(epochs)
         for e in xrange(epochs):
             all_prev.append(X_bar)
-            dW, dX_bar, p_prod = self.compute_grads(X, X_bar, W)
+            dW, dX_bar = self.compute_grads(X, X_bar, W)
             W -= lr*dW
             X_bar = X_bar - lr*dX_bar
-            gradients = Gradients(p_prod, dW, dX_bar)
-        return W, X_bar, all_prev, gradients
+            all_dW[e] = np.mean(abs(dW))
+            all_dX_bar[e] = np.mean(abs(dX_bar))
+        return W, X_bar, all_prev, (all_dW, all_dX_bar)
 
     def compute_grads(self, X, X_bar, W):
         p = self._compute_p(W)
@@ -26,18 +29,22 @@ class GDC:
         #Gradient w.r.t the weights
         dist_sq = x_diff ** 2.0
         #Scale by average p *(1 - p)
-        p_prod = p * (1 - p)
-        pp_mean = np.mean(p_prod) #+ 0.0000001
-        dW = p_prod/pp_mean * np.sum(dist_sq, axis=2)
-        #Gradient w.r.t the centres
+        #p_prod = p * (1 - p)
         n, K = p.shape
         _, d = X_bar.shape
         p_broadcast = p.reshape(n, K, 1)
+        sum_dist_sq = np.sum(dist_sq, axis=2)
+        C = p * sum_dist_sq
+        #pp_mean = np.mean(p_prod) #+ 0.0000001
+        dW = p * (sum_dist_sq - np.sum(C, axis=1).reshape((n, 1)))
+        #dW = p_prod * np.sum(dist_sq, axis=2) #/pp_mean
+        dW = dW/(np.mean(abs(dW)))
+        #Gradient w.r.t the centres
         dX_bar = np.sum(p_broadcast * -x_diff, axis = 0) * 2.0
         over_n = 1.0/float(n)
         over_d = 1.0/float(d)
-        c = over_d
-        return c * dW, c * dX_bar, p_prod/pp_mean
+        c = 1.0 #over_d
+        return c * dW, c * dX_bar #, p_prod#/pp_mean
 
     def cost(self, X, W, X_bar):
         n, K = W.shape

src/main.py
@@ -1,4 +1,4 @@
-from cluster_gen import k_gaussian_clusters
+from cluster_gen import *
 from grad_desc_clustering import GDC
 from convex_opt import *
 from centre_animator import animate
@@ -7,10 +7,11 @@ import numpy as np
 import cluster_utils
 
 K = 4
-lr = 0.18
-epochs = 120
+lr = 0.1
+epochs = 200
 # Generate the clusters
-clusters, actual_centres, global_opt = k_gaussian_clusters(K)
+#clusters, actual_centres, global_opt = k_gaussian_clusters(K)
+clusters, actual_centres, global_opt = k_centres_d_space(K, 2, 5)
 X = np.concatenate(clusters, axis=0)
 
 # Choose K for GDC
@@ -23,7 +24,7 @@ gdc = GDC()
 X_bar = np.random.uniform(low= -5.0, high=5.0, size=(K, 2))
 
 # Train the clustering algorithms
-W, X_bar_gdc, all_prev_gdc, last_grads = gdc.train(X, K, lr, epochs, X_bar)
+W, X_bar_gdc, all_prev_gdc, all_grads = gdc.train(X, K, lr, epochs, X_bar)
 
 kMeans = k_means.KMeans()
 X_bar_kMeans, all_prev_kMeans = kMeans.train(X, K, epochs, X_bar)

src/main_large_cluster_test.py
@@ -1,62 +1,20 @@
-from cluster_gen import *
-from grad_desc_clustering import GDC
-from convex_opt import *
-import k_means
-import numpy as np
-import cluster_utils
-#from centre_animator import animate
+from experiment_runner import *
+from gradient_viewer import *
 
 
 
 K = 10
-d = 4
-lr = 0.18
+d = 2
+lr = 0.2
 epochs = 200
+n_per_cluster = 5
+num_runs = 30
 
-total_opt = 0
-total_gdc = 0
-total_kMeans = 0
-gdc_under = 0
-kMeans_under = 0
-
-for i in xrange(100):
-    # Generate the clusters
-    clusters, actual_centres, global_opt = k_centres_d_space(K, d, 5)
-    X = np.concatenate(clusters, axis=0)
-
-    # Choose K for GDC
-    gdc = GDC()
-    # co = ConvexOptimizer(gdc)
-    # tp = TrainingParams(X, lr, epochs)
-    # K, _ = co.bin_search(tp, 0, 10)
-
-    # Create same set of starting centres
-    X_bar = np.random.uniform(low=-5.0, high=5.0, size=(K, d))
-
-    # Train the clustering algorithms
-    W, X_bar_gdc, all_prev_gdc, last_grads = gdc.train(X, K, lr, epochs, X_bar)
-
-    kMeans = k_means.KMeans()
-    X_bar_kMeans, all_prev_kMeans = kMeans.train(X, K, epochs, X_bar)
-
-    # Compute total cost of each clustering alg
-    gdc_C = cluster_utils.cost_of_closest_to(X, X_bar_gdc)
-    kMeans_C = cluster_utils.cost_of_closest_to(X, X_bar_kMeans)
-
-    #Update agregates
-    total_opt += global_opt
-    total_gdc += gdc_C
-    total_kMeans += kMeans_C
-    if gdc_C < global_opt:
-        gdc_under += 1
-    if kMeans_C < global_opt:
-        kMeans_under += 1
-
-print "Global Optimum: "+str(total_opt/100.0)
-print "GDC: "+str(total_gdc/100.0)+"  Percent Under: "+str(gdc_under)
-print "K Means: "+str(total_kMeans/100.0)+"  Percent Under: "+str(kMeans_under)
+all_grads = run_and_report_grads(K, d, n_per_cluster, lr, epochs, num_runs)
+display_grads(all_grads, epochs)
 
 
-# print "Global Optimum: "+str(global_opt)
-# print "GDC: "+str(gdc_C)
-# print "K Means: "+str(kMeans_C)
+
+
+
+